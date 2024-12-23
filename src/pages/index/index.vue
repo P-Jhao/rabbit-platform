6012ab67ddbc3d982ac5fb5672bc89acbabb1075
P-Jhao <2212688637@qq.com>
2024-12-23 18:36:13 +0800

    封装前台分类组件

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { getHomeBannerAPI } from '@/services/home'
 import CustomNavbar from './components/CustomNavbar.vue'
+import CategoryPanel from './components/CategoryPanel.vue'
 import { onLoad } from '@dcloudio/uni-app'
 import { ref } from 'vue'
 import type { BannerItem } from '@/types/banner'
@@ -20,9 +21,12 @@ onLoad(() => {
 <template>
   <CustomNavbar />
   <XtxSwiper :list="bannerListRef" />
+  <CategoryPanel />
   <view class="index">index</view>
 </template>
 
 <style lang="scss">
-//
+page {
+  background: #f7f7f7;
+}
 </style>
