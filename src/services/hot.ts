517dd9493c2d38f2148eef2352612baa2af8fd3f
P-Jhao <2212688637@qq.com>
2024-12-24 19:02:20 +0800

    热门推荐类型定义

--- src/services/hot.ts
+++ src/services/hot.ts
@@ -1,10 +1,11 @@
 import { http } from '@/utils/http'
 import type { PageParams } from '../types/global'
+import type { HotResult } from '@/types/hot'
 
 type HotParams = PageParams & { subType?: string }
 
 export const getHotComponentData = async (url: string, data?: HotParams) => {
-  return await http({
+  return await http<HotResult>({
     method: 'GET',
     url,
     data,
