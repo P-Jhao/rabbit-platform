6d0209e6b990bb494afd9cd7bb2aa8379040d7ee
P-Jhao <2212688637@qq.com>
2024-12-25 15:20:28 +0800

    商品详情-页面渲染

--- src/services/goods.ts
+++ src/services/goods.ts
@@ -1,7 +1,8 @@
+import type { GoodsResult } from '@/types/goods'
 import { http } from '@/utils/http'
 
 export const getGoodsByIdAPI = async (id: string) => {
-  return await http({
+  return await http<GoodsResult>({
     method: 'GET',
     url: '/goods',
     data: { id },
